941a07d032887988ac05cdb316c45b337034ba50
Lin <dlin7253@gmail.com>
2019-07-13 20:16:03 +0800

    create_database

---
+++ app/src/main/java/com/example/coolweather/db/Country.java
@@ -0,0 +1,47 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/*****县表******/
+public class Country extends DataSupport {
+
+    private int id;
+
+    private String countryName;
+
+    private int cityCode;
+
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
